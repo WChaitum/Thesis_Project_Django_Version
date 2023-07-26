3c442682b319e2012f52e466246af6c53a2c3820
WChaitum <89720701+WChaitum@users.noreply.github.com>
2023-07-26 22:26:55 +0700

    ...

--- Thesis/ThesisApp/models.py
+++ Thesis/ThesisApp/models.py
@@ -4,8 +4,12 @@ class Person(models.Model):
     Parent_name = models.CharField(max_length=100)
     Parent_key = models.CharField(max_length=100)
     children_name = models.CharField(max_length=100)
-    status = models.CharField(max_length=100 )
-    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=100)
+    time = models.TimeField(default='00:00:00')  # กำหนดค่าเริ่มต้นเป็น '00:00'
+    Parent_image = models.ImageField(upload_to='Images',blank=True)
+    children_image = models.ImageField(upload_to='Images',blank=True)
 
-    # def __str__(self):
-    #     return "Name : "+self.name +" "+"Age : "+str(self.age)
+    # โค้ดอื่นๆ ของโมเดล Person ที่คุณมีอยู่
+
+    def __str__(self):
+        return "Name : "+self.Parent_name +" "+"Tine : "+str(self.time)
--- Thesis/ThesisApp/views.py
+++ Thesis/ThesisApp/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render,redirect
 from ThesisApp.models import Person
 from django.contrib import messages
+from django.utils import timezone
 
 # Create your views here.
 
 # การแสดงข้อมูล
 def index(request):
-    all_person = Person.objects.filter(status="Call").first()
+    all_person = Person.objects.filter(status="Call").order_by('time').first()
     if request.method == 'POST':
         search_keyword = request.POST.get('search_keyword', '')
         person = Person.objects.filter(Parent_key=search_keyword).first()
         if person:
             person.status = 'Call'
+            person.time= timezone.now()  # เพิ่มบรรทัดนี้เพื่อกำหนดค่า date เป็นค่าปัจจุบัน
             person.save()
             messages.success(request, f'ได้รับข้อมูลของ คุณ{person.Parent_name} เรียบร้อยแล้ว')
             return redirect('/')
@@ -37,6 +39,7 @@ def member(request):
 def about(request):
     return render(request,"about.html")
 
+
 # การเพิ่มข้อมูล
 def form(request):
     if request.method == "POST":
@@ -44,6 +47,8 @@ def form(request):
         Parent_Name = request.POST.get("Parent_Name")
         Children_Name = request.POST.get("Children_Name")
         Parent_Key = request.POST.get("Parent_Key")
+        Parent_Img = request.FILES.get("Parent_File")
+        Children_Img = request.FILES.get("Children_File")
 
         if Parent_Name and Children_Name and Parent_Key:
             print("Parent Name: " + Parent_Name, "Children Name: " + Children_Name)
@@ -52,8 +57,9 @@ def form(request):
                 Parent_name=Parent_Name,
                 Parent_key=Parent_Key,
                 children_name=Children_Name,
-                status = "New"
-                
+                status="New",
+                Parent_image=Parent_Img,  # ให้แน่ใจว่ามีตัวแปร Parent_image ใน model Person
+                children_image=Children_Img,  # ให้แน่ใจว่ามีตัวแปร children_image ใน model Person
             )
             messages.success(request, "บันทึกข้อมูลเรียบร้อย")
             return redirect('/member')
@@ -67,19 +73,27 @@ def form(request):
 
 
 # การแก้ไขข้อมูล
-def edit(request,person_id):
+def edit(request, person_id):
     if request.method == "POST":
         person = Person.objects.get(id=person_id)
         person.Parent_name = request.POST["Parent_Name"]
         person.children_name = request.POST["Children_Name"]
         person.Parent_key = request.POST["Parent_Key"]
+
+        # ตรวจสอบว่ามีการอัปโหลดไฟล์ Parent_File และ Children_File หรือไม่
+        if 'Parent_File' in request.FILES:
+            person.Parent_image = request.FILES['Parent_File']
+        if 'Children_File' in request.FILES:
+            person.children_image = request.FILES['Children_File']
+
         person.save()
-        messages.success(request,"อัพเดทข้อมูลเรียบร้อย")
+        messages.success(request, "อัพเดทข้อมูลเรียบร้อย")
         return redirect('/member')
     else:
         # ดึงข้อมูลที่ต้องการแก้ไข
         person = Person.objects.get(id=person_id)
-        return render(request,"edit.html",{"person":person})
+        return render(request, "edit.html", {"person": person})
+
     
 
 # การลบข้อมูล  
